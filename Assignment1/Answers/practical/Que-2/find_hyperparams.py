91509367d818c70a6fe218e399c2cfce185188b5
dhaivat666 <dhaivat1994@gmail.com>
2020-02-09 22:09:19 -0500

    to get csv of hyperparam stats.

--- Assignment1/Answers/practical/Que-2/find_hyperparams.py
+++ Assignment1/Answers/practical/Que-2/find_hyperparams.py
@@ -1,7 +1,38 @@
 import numpy as np
+import csv
+
 
 hype = np.load('hyperparameter_search.npy', allow_pickle=True)
 
+f = open('hyperparams_best.csv', 'w')
+
+
+rows = []
 for config in hype:
 	if config['validation_accuracy'][9] > 0.97:
-		print(config)	
+		out = [config['hidden_dims'], config['lr'], config['activation'], config['train_accuracy'][9], config['validation_accuracy'][9], config['model_params']]
+		rows.append(out)
+
+main_row = ['hidden_dims', 'learning rate', 'activation function', 'training accuracy', 'validation_accuracy', 'model parameters']
+
+with f:
+	writer = csv.writer(f)
+	writer.writerow(main_row)
+	for row in rows:
+		writer.writerow(row)
+
+f = open('hyperparams_full.csv', 'w')
+
+
+rows = []
+for config in hype:
+	out = [config['hidden_dims'], config['lr'], config['activation'], config['train_accuracy'][9], config['validation_accuracy'][9], config['model_params']]
+	rows.append(out)
+
+main_row = ['hidden_dims', 'learning rate', 'activation function', 'training accuracy', 'validation_accuracy', 'model parameters']
+
+with f:
+	writer = csv.writer(f)
+	writer.writerow(main_row)
+	for row in rows:
+		writer.writerow(row)
